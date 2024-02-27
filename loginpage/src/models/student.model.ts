b87475a356d27627fd90e7cc268b7ee95fb946b8
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2024-02-27 22:39:32 +0530

    first commit

---
+++ loginpage/src/models/student.model.ts
@@ -0,0 +1,43 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Student extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+
+  [prop: string]: any;
+
+  constructor(data?: Partial<Student>) {
+    super(data);
+  }
+}
+
+export interface StudentRelations {
+  // describe navigational properties here
+}
+
+export type StudentWithRelations = Student & StudentRelations;
